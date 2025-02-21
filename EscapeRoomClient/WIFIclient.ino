f1615de9d02ef6fd781a332e4154f31e550b09a9
aviguedj <aviguedj555@gmail.com>
2025-02-21 10:48:04 +0200

    initial connection to arduino WIFI
    initial connection to arduino WIFI

--- EscapeRoomClient/WIFIclient.ino
+++ EscapeRoomClient/WIFIclient.ino
@@ -3,51 +3,41 @@
 #include <WiFiUdp.h>
 #include <ESP8266HTTPClient.h>
 
-const char* ssid = "Project";
+const char* ssid = "project";
 const char* pswd = "88888888";
 
 WiFiClient client;
 int server_port = 80; 
 
+unsigned long previousMillis = 0;
+
+
 void wifi_Setup() {
   Serial.println("wifiSetup");
-  WiFi.begin(ssid);
-  //    WiFi.begin(ssid, pswd);
+     WiFi.begin(ssid, pswd);
+     previousMillis = millis();
 
   while (WiFi.status() != WL_CONNECTED) {
     Serial.println("trying ...");
-    delay(100);
+    if(millis() - previousMillis >= 100){
+      previousMillis = millis();
+    }
+   
   }
   Serial.println("Connected to network");
+
+  SendData();
 }
 
-void SendData(int val) {
+
+void SendData(int digit) {
   HTTPClient http;
-  String dataURL = "";
-  dataURL += "ACT=SET&DEV=1121&CH=1";
-  dataURL += "&VAL=" + String(val);
-  http.begin(client, "55.55.55.55/GEN/api.php?" + dataURL);
+  http.begin(client, "http://55.55.55.55/addDigit?digit=" + digit);
   int httpCode = http.GET();
   Serial.println(httpCode);
   http.end();
 }
 
-int GetData() {
-  int ret = -1;
-  HTTPClient http;
-  String dataURL = "";
-  dataURL += "ACT=GET&DEV=177944&CH=55";
-  http.begin(client, "http://api.kits4.me/GEN/api.php?" + dataURL);
-  int httpCode = http.GET();
-  Serial.println(httpCode);
-  if (httpCode == HTTP_CODE_OK) {
-    Serial.print("HTTP response code ");
-    Serial.println(httpCode);
-    String Res = http.getString();
-    Serial.println(Res);
-    ret = Res.toInt();
-  }
-  http.end();
 
-  return ret;
-}
+
+
